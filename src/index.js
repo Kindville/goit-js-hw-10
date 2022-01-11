e5261287132ce677ba4f7a3274a9bb19fb1ff95f
Kseniia <kceniawhite@gmail.com>
2022-01-12 00:16:38 +0200

    add base files

--- src/index.js
+++ src/index.js
@@ -1,45 +1,14 @@
 import './css/styles.css';
-import countryCard from './country-card.hbs';
-import countryCards from './countryList.hbs';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './fetchCountries'
 
-import API from'./fetchCountries';
-const debounce = require('lodash.debounce');
+const searchForm = document.querySelector('#search-form')
+const searchBtn = document.querySelector('button')
 
-const DEBOUNCE_DELAY = 300;
+searchBtn.addEventListener('input', onInputclick)
 
-const inputEl = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-
-inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
-
-function onSearch(e) {
-  e.preventDefault();
-  const formValue = e.target.value.trim();
-  if (formValue === '') {
-     return  clearCountryCard();
-  }
-  
-  API.fetchCountries(formValue).then(data => {
-    clearCountryCard();
-     if (data.length > 10) {
-    return  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    
-    } else if (data.length === 1) {
-      renderCountryCard(data, countryCard);
-    } else if (data.length <= 10) {
-      renderCountryCard(data, countryCards);
-    }
-  })
-  .catch(error =>
-         error);
-}
-
-function renderCountryCard(countries, template) {
-  const markUp = countries.map(country => template(country)).join('');
-  countryList.innerHTML = markUp;
-}
-function clearCountryCard() {
-  countryList.innerHTML ='';
+function onInputclick(e) {
+  e.preventDefault;
+  fetchPictures()
 }
